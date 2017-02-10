5abf5b7acf34098b34adcef8023c57c6c274c72c
plokamar1 <p.dorgiak@gmail.com>
2017-02-10 16:56:36 +0200

    I catch the exceptions for false file name inputs and also added some comments

--- csvErrors.py
+++ csvErrors.py
@@ -1,4 +1,3 @@
-import csv
 import sys
 import os
 
@@ -7,16 +6,27 @@ inputFile = ''
 data = ''
 outputFile = ''
 path = os.path.dirname(os.path.realpath(__file__))+"\\"
+#Clean previous commands in the command line
+for i in range(0,10):
+    print('\n')
+#User gives the input files
 if len(sys.argv) == 1:
     inputFile = path + input('Please give me the input file with the accounts:\n')
-    data = path + input('Please give me the file with the emails you want me to search:\n')
+    data = path + input('\nPlease give me the file with the emails you want me to search:\n')
 else:
     inputFile = str(sys.argv[1])
     data = str(sys.argv[2])
+#Try opening the files given
+try:
+    f= open(inputFile)
+except IOError:
+    print('There is not such file or directory for input file')
+try:
+    n = open(data)
+except IOError:
+    print('There is not such file or directory for data file')
 
-f= open(inputFile)
-n = open(data)
-results = open('finish.txt', 'w');
+results = open('finish.txt', 'w')
 accData = f.readlines()
 dataArrray = n.readlines()
 results.truncate()#clean the file with the results
