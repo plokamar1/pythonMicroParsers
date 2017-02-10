9d65db8ab98b2a3fe6754168eafca6b018226031
plokamar1 <p.dorgiak@gmail.com>
2017-02-10 13:53:56 +0200

    Changed the variables to something better

--- csvErrors.py
+++ csvErrors.py
@@ -9,26 +9,24 @@ outputFile = ''
 path = os.path.dirname(os.path.realpath(__file__))+"\\"
 if len(sys.argv) == 1:
     inputFile = path + input('Please give me the input file with the accounts:\n')
-    print(inputFile)
     data = path + input('Please give me the file with the emails you want me to search:\n')
-    print(data)
 else:
     inputFile = str(sys.argv[1])
     data = str(sys.argv[2])
 
 f= open(inputFile)
-firstdata = f.readlines()
 n = open(data)
-newdata = n.readlines()
-finished = open('finish.txt','w');
-finished.truncate()
-for nline in newdata:
-    nline = nline.replace('\n','')
-    nline = nline.replace('\t', '')
-    for fline in firstdata:
-        if nline in fline:
-            finished.write(fline)
+results = open('finish.txt', 'w');
+accData = f.readlines()
+dataArrray = n.readlines()
+results.truncate()#clean the file with the results
+for dataLine in dataArrray:
+    dataLine = dataLine.replace('\n', '')
+    dataLine = dataLine.replace('\t', '')
+    for accLine in accData:
+        if dataLine in accLine:
+            results.write(accLine)
 
 f.close()
 n.close()
-finished.close()
+results.close()
