04e04a468d280d69c811de49bd1b64c191cb5a35
plokamar1 <p.dorgiak@gmail.com>
2017-02-10 13:38:37 +0200

    Now the filepath can be given by arguements or by user input

--- csvErrors.py
+++ csvErrors.py
@@ -1,13 +1,30 @@
 import csv
-f= open('Updatefinal.txt')
+import sys
+import os
+
+
+inputFile = ''
+data = ''
+outputFile = ''
+path = os.path.dirname(os.path.realpath(__file__))+"\\"
+if len(sys.argv) == 1:
+    inputFile = path + input('Please give me the input file with the accounts:\n')
+    print(inputFile)
+    data = path + input('Please give me the file with the emails you want me to search:\n')
+    print(data)
+else:
+    inputFile = str(sys.argv[1])
+    data = str(sys.argv[2])
+
+f= open(inputFile)
 firstdata = f.readlines()
-n = open('updatedErrors.txt')
+n = open(data)
 newdata = n.readlines()
 finished = open('finish.txt','w');
 finished.truncate()
 for nline in newdata:
     nline = nline.replace('\n','')
-    nline = nline.replace('\t','')
+    nline = nline.replace('\t', '')
     for fline in firstdata:
         if nline in fline:
             finished.write(fline)
@@ -15,5 +32,3 @@ for nline in newdata:
 f.close()
 n.close()
 finished.close()
-
-        
